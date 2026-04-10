a0aef0d86cb9e5eb3ae08a2b2ab60907444c951c
manuel@-2.6 <manuel@masiasweb.com>
2026-04-10 11:54:50 +0200

    feat: clase Tablero completada! (de momento...)

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,19 +1,67 @@
+class Tablero {
 
-public class Tablero {
+    private char[][] casillas;
+    final private int DIMENSION = 3;
+    private Console console;
+
+    public Tablero() {
+        casillas = new char[DIMENSION][DIMENSION];
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                casillas[i][j] = '_';
+            }
+        }
+        console = new Console();
+    }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                console.write(" " + casillas[i][j]);
+            }
+            console.writeln();
+        }
     }
 
     public boolean hayTresEnRaya() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hayTresEnRaya'");
+        return this.hayTresEnRaya('x') || this.hayTresEnRaya('o');
+    }
+
+    private boolean hayTresEnRaya(char color) {
+        int[] filas = new int[3];
+        int[] columnas = new int[3];
+        int diagonal = 0;
+        int secundaria = 0;
+
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (casillas[i][j] == color) {
+                    filas[i]++;
+                    columnas[j]++;
+                    diagonal = diagonal + ((i == j) ? 1 : 0);
+                    secundaria = secundaria + ((i + j == 2) ? 1 : 0);
+
+                    if (filas[i] == 3 || columnas[j] == 3 || diagonal == 3 || secundaria == 3) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+        return false;
+
     }
 
     public boolean estaCompleto(Jugador jugador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'estaCompleto'");
+        assert jugador != null;
+        int conteoFichas = 0;
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (casillas[i][j] == jugador.color()) {
+                    conteoFichas++;
+                }
+            }
+        }
+        return conteoFichas == 3;
     }
-
 }
